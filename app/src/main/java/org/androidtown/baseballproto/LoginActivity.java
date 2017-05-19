ab6a1bd1d177774d82229c73ea206190990bcf5b
hmbr1003 <phm12171@naver.com>
2017-05-19 21:18:44 +0900

    Initial commit

---
+++ app/src/main/java/org/androidtown/baseballproto/LoginActivity.java
@@ -0,0 +1,247 @@
+package org.androidtown.baseballproto;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.androidtown.baseballproto.databinding.ActivityLoginBinding;
+
+import butterknife.BindView;
+
+/**
+ * Created by Administrator on 2017-05-13-013.
+ */
+
+public class LoginActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener {
+    public static final int GOOGLE_SIGN_IN = 9001;
+    GoogleApiClient mGoogleApiClient;  //구글 로그인 관련
+    private CallbackManager mCallbackManager;   //페이스북 로그인 관련
+
+    private FirebaseAuth mAuth;         //파이어베이스 계정 관련
+    private FirebaseDatabase database;  //파이어베이스 DB 관련
+    private DatabaseReference myRef;    //파이어베이스 DB 관련
+    ActivityLoginBinding activityLoginBinding;  //데이터 바인딩
+
+    ProgressDialog dialog;
+    Toolbar mToolbar; //뒤로가기 버튼 만들기
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());     //페이스북 SDK 연동
+        activityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login);    //데이터바인딩
+
+        //뒤로가기 버튼 만들기
+        activityLoginBinding.toolBar.setTitle("로그인");         //타이틀입니다.
+        activityLoginBinding.toolBar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(activityLoginBinding.toolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        mAuth = FirebaseAuth.getInstance();     //파이어베이스 계정 인스턴스 가져오기
+        // DB 관련 변수 초기화
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("users");
+
+        //구글 로그인 작업
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("886923317230-ksfdurhset6ak2hm20t8hjcad96oe0k3.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        findViewById(R.id.button_google_login).setOnClickListener(new View.OnClickListener() {   //구글 로그인 버튼을 클릭 했을 때의 동작 설정
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, GOOGLE_SIGN_IN);       //구글 로그인 연동 액티비티를 결과를 받는 형식으로 띄움
+            }
+        });
+
+
+        //페이스북 로그인 작업
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton facebookLoginButton = (LoginButton) findViewById(R.id.button_facebook_login);
+        facebookLoginButton.setReadPermissions("email", "public_profile");  //사용자에게서 가져올 정보 권한 설정
+        facebookLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {    //페이스북 로그인을 성공했을 시
+                Toast.makeText(LoginActivity.this, "페이스북 계정 연결 성공", Toast.LENGTH_SHORT).show();
+                dialog=new ProgressDialog(LoginActivity.this);
+                dialog.setProgress(ProgressDialog.STYLE_SPINNER);
+                dialog.setMessage("로그인 중입니다...");
+                dialog.show();
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(LoginActivity.this, "페이스북 로그인 취소", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Toast.makeText(LoginActivity.this, "페이스북 로그인 에러, 에러 내용 : "+e, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void handleFacebookAccessToken(AccessToken token) { //페이스북 계정을 파이어베이스에 등록하는 함수
+        // [START_EXCLUDE silent]
+        // [END_EXCLUDE]
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        dialog.dismiss();
+                        if (task.isSuccessful()) {  //등록 성공했을 시
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("파이어베이스 페이스북 계정","등록 성공");
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();    //유저 정보를 가져와서
+                            if(user.getEmail()==null) {                                         //이메일을 허용했는 지 검사한 후
+                                Toast.makeText(getApplicationContext(), "이메일을 허용하시고 다시 로그인해주세요.", Toast.LENGTH_SHORT).show();
+                                user.delete()                                                   //안했으면 등록했던 계정을 삭제한다.
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Log.d("파이어 베이스 페이스북 계정", "삭제");
+                                                }
+                                            }
+                                        });
+                                mAuth.signOut();                                                //그 후 파이어베이스 로그아웃
+                                LoginManager.getInstance().logOut();                            //페이스북 로그아웃 한다 유저는 로그인을 다시해야한다.
+                            }
+                            else {
+                                //이메일을 제공했을 시 데이터베이스에 유저 정보 등록 후
+                                database = FirebaseDatabase.getInstance();
+                                myRef =  FirebaseDatabase.getInstance().getReference();
+                                myRef.child("users").child(user.getUid()).child("name").setValue(user.getDisplayName());
+                                myRef.child("users").child(user.getUid()).child("email").setValue(user.getEmail());
+
+                                //홈화면으로 돌아가는 작업을 한다.
+                                Intent intent = new Intent();
+                                setResult(RESULT_OK, intent);           //인텐트로 OK값을 보내고 홈 화면에서는 온액티비티리슐트로 받는다.
+                                finish();                               //창 닫기
+                            }
+
+                        } else {
+                            // 계정 등록 실패 시
+                            Log.w("파이어 베이스 페이스북 계정", "등록 실패", task.getException());
+                            Toast.makeText(LoginActivity.this, "계정 등록 실패",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+
+    //새로 띄운 액티비티에서 결과를 받아온 경우의 동작을 설정
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GOOGLE_SIGN_IN) {    //구글 인증 액티비티가 응답을 보내왔을 경우
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Signed in successfully, show authenticated UI.
+                GoogleSignInAccount acct = result.getSignInAccount();
+                Toast.makeText(LoginActivity.this, "구글 계정 연결 성공", Toast.LENGTH_SHORT).show();
+                dialog=new ProgressDialog(this);
+                dialog.setProgress(ProgressDialog.STYLE_SPINNER);
+                dialog.setMessage("로그인 중입니다...");
+                dialog.show();
+                firebaseAuthWithGoogle(acct);
+            } else {
+                // Signed out, show unauthenticated UI.
+                Toast.makeText(LoginActivity.this, "구글 계정 연결 실패", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {                  //구글이 아닐 경우엔 페이스북에 전달한다.
+            // Pass the activity result back to the Facebook SDK
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) { //구글 계정을 파이어베이스에 연동하는 동작을 하는 함수
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        dialog.dismiss();
+                        if (task.isSuccessful()) {
+                            Log.d("구글 로그인", "파이어베이스 연동 성공");
+
+                            //데이터베이스에 유저정보 등록
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            database = FirebaseDatabase.getInstance();
+                            myRef =  FirebaseDatabase.getInstance().getReference();
+                            myRef.child("users").child(user.getUid()).child("name").setValue(user.getDisplayName());
+                            myRef.child("users").child(user.getUid()).child("email").setValue(user.getEmail());
+
+                            //창 닫기
+                            finish();
+                        } else {
+                            Log.w("구글 로그인", "파이어베이스 연동 실패", task.getException());
+                            Toast.makeText(LoginActivity.this, "로그인 실패",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    //구글 로그인 관련 리스너
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "연결 실패", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/org/androidtown/baseballproto/MainActivity.java
@@ -0,0 +1,378 @@
+package org.androidtown.baseballproto;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
+import android.util.Base64;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.androidtown.baseballproto.databinding.ActivityMainBinding;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import devlight.io.library.ntb.NavigationTabBar;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    public static final int LOGIN_REQUEST = 100;
+    HomeFragment homeFragment;
+    DeliveryFragment deliveryFragment;
+    TakeoutFragment takeoutFragment;
+    BaseInfoFragment baseInfoFragment;
+    ViewPager viewPager;
+
+
+    ActivityMainBinding mainBinding;
+    TextView userEmail;
+    TextView userName;
+    MenuItem navLogin;
+//    MenuItem navLogout;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
+        homeFragment = new HomeFragment();
+        deliveryFragment = new DeliveryFragment();
+        takeoutFragment = new TakeoutFragment();
+        baseInfoFragment = new BaseInfoFragment();
+
+        initUI();  //하단 UI 세팅
+
+        //상단 UI 세팅
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mainBinding.drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mainBinding.drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        //왼쪽 슬라이드 메뉴 로그인과 로그아웃버튼 아이디 할당
+        Menu menu = navigationView.getMenu();
+        navLogin=menu.findItem(R.id.nav_login);
+//        navLogout=menu.findItem(R.id.nav_logout);
+
+        //왼쪽 슬라이드 메뉴 유저이메일과 유저 이름 아이디 할당
+        View headerView = navigationView.getHeaderView(0);
+        userEmail = (TextView) headerView.findViewById(R.id.userEmail);
+        userName = (TextView) headerView.findViewById(R.id.userName);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    navLogin.setTitle("로그아웃");
+                    userName.setText(user.getDisplayName());
+                    userEmail.setText(user.getEmail());
+                } else {
+                    navLogin.setTitle("로그인");
+                    userName.setText("로그인이 필요합니다");
+                    userEmail.setText("");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+    //    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+    //좌측 네비게이션 바
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_login) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if(user==null) {
+                Intent intent = new Intent(this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivityForResult(intent, LOGIN_REQUEST);
+            }
+            else {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("로그아웃 확인");
+                builder.setMessage("로그아웃 하시겠습니까?");
+                builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mAuth.signOut();
+                        LoginManager.getInstance().logOut();
+                        Toast.makeText(MainActivity.this, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        } else if (id == R.id.nav_cart) {  //왼쪽 슬라이드메뉴 장바구니 부분
+
+        } else if (id == R.id.nav_orderList) {  //왼쪽 슬라이드메뉴 주문내역 부분
+
+        } else if (id == R.id.nav_reviewManage) {  //왼쪽 슬라이드메뉴 리뷰관리 부분
+
+        } else if (id == R.id.nav_changeCol) {  //왼쪽 슬라이드메뉴 경기장 변경 부분
+
+        } else if (id == R.id.nav_newBusiness) {  //왼쪽 슬라이드메뉴 사업자 신규 등록 부분
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //프래그먼트 어댑터
+    private class FragmentAdapter extends FragmentStatePagerAdapter {
+
+        public FragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new HomeFragment();
+                case 1:
+                    return new DeliveryFragment();
+                case 2:
+                    return new TakeoutFragment();
+                case 3:
+                    return new BaseInfoFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+    }
+
+    @Override
+    public void onStart() { //온스타트 시 파이어베이스 계정 객체에 리스너 부착
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {  //온스탑 시 파이어베이스 계정 객체에 리스너 제거
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+    //하단 네비게이션 바 활성화 함수
+    private void initUI() {
+        viewPager = (ViewPager) findViewById(R.id.vp_horizontal_ntb);
+        viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+//        viewPager.setAdapter(new PagerAdapter() {
+//            @Override
+//            public int getCount() {
+//                return 4;
+//            }
+//
+//            @Override
+//            public boolean isViewFromObject(final View view, final Object object) {
+//                return view.equals(object);
+//            }
+//
+//            @Override
+//            public void destroyItem(final View container, final int position, final Object object) {
+//            }
+//
+//            @Override   //이부분에 프래그먼트로 변경 필요
+//            public Object instantiateItem(final ViewGroup container, final int position) {
+//                final View view;
+//                switch (position) {
+//                    case 0:
+//                        view = LayoutInflater.from(getBaseContext()).inflate(R.layout.fragment_home, null, false);
+//                        container.addView(view);
+//                        return view;
+//                    case 1:
+//                        view = LayoutInflater.from(getBaseContext()).inflate(R.layout.fragment_delivery, null, false);
+//                        container.addView(view);
+//                        return view;
+//                    case 2:
+//                        view = LayoutInflater.from(getBaseContext()).inflate(R.layout.fragment_takeout, null, false);
+//                        container.addView(view);
+//                        return view;
+//                    case 3:
+//                        view = LayoutInflater.from(getBaseContext()).inflate(R.layout.fragment_baseinfo, null, false);
+//                        container.addView(view);
+//                        return view;
+//                    default:
+//                        return null;
+//                }
+//                final View view = LayoutInflater.from(
+//                        getBaseContext()).inflate(R.layout.item_vp, null, false);
+//
+//                container.addView(view);
+//                return view;
+//            }
+//        });
+
+        final String[] colors = getResources().getStringArray(R.array.default_preview);
+
+        final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_horizontal);
+        final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_first),
+                        Color.parseColor(colors[0]))
+//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_sixth))
+                        .title("홈")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_second),
+                        Color.parseColor(colors[1]))
+//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("배달음식")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_third),
+                        Color.parseColor(colors[2]))
+//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_seventh))
+                        .title("테이크아웃")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_fourth),
+                        Color.parseColor(colors[3]))
+//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+                        .title("야구 정보")
+                        .build()
+        );
+
+        navigationTabBar.setModels(models);
+        navigationTabBar.setViewPager(viewPager, 0);
+        navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(final int position) {
+//                navigationTabBar.getModels().get(position).hideBadge();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(final int state) {
+
+            }
+        });
+
+        navigationTabBar.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < navigationTabBar.getModels().size(); i++) {
+                    final NavigationTabBar.Model model = navigationTabBar.getModels().get(i);
+                    navigationTabBar.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            model.showBadge();
+                        }
+                    }, i * 100);
+                }
+            }
+        }, 500);
+    }
+}
+
